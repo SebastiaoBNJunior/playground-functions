e35201a4d11b462621a3f8e3b5672e75ca2ec3f9
SebastiaoBNJunior <junin_sbn@hotmail.com>
2023-04-14 16:49:07 -0300

    Finalizado exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,39 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  
+  for (let i = 0; i < numbers.length; i++) {
+    const currentNumber = numbers[i];
+    if (currentNumber < 0 || currentNumber > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (currentNumber === numbers[j]) {
+        if (j - i < 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+        break;
+      }
+    }
+  }
+  
+  const ddd = numbers.slice(0, 2).join('');
+  const firstPart = numbers.slice(2, 7).join('');
+  const secondPart = numbers.slice(7).join('');
+  
+  return `(${ddd}) ${firstPart}-${secondPart}`;
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])); // retorna (12) 34567-8901
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // retorna 'Array com tamanho incorreto.'
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1])); // retorna 'não é possível gerar um número de telefone com esses valores'
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 5, 5, 8, 9, 0, 1])); // retorna 'não é possível gerar um número de telefone com esses valores'
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])); // retorna (12) 34567-8901
+console.log(generatePhoneNumber([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])); // retorna (00) 00000-0000
+console.log(generatePhoneNumber([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9])); // retorna (98) 76540-1099
+
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
